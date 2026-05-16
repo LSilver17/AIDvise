e71e25b3983ab7958d730d201ca7eca9be556fb2
unknown <sbcollins29@gmail.com>
2026-05-16 18:02:16 -0400

    Seperated authOptions from endpoint

--- frontend/app/api/auth/[...nextauth]/route.ts
+++ frontend/app/api/auth/[...nextauth]/route.ts
@@ -6,71 +6,7 @@ Authors:   Sean Collins, NextAuth
 GitHub:   https://github.com/LSilver17/CSC212---AI-Agent
 =============================================================================*/
 import NextAuth from "next-auth";
-import type { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { signIn } from "next-auth/react";
-
-import { validate_credentials } from "@/app/lib/account/account_db_utils";
-
-/**
- * API endpoint for any NextAuth requests via hooks like {@link signIn}. Currently credentials
- * are the only supported provider. Checks provided credentials and matches them against
- * those stored in the database with {@link validate_credentials}.
- */
-export const authOptions: NextAuthOptions = {
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                username: { label: "Username", type: "text", placeholder: "johndoe"},
-                password: { label: "Password", type: "password" }
-            },
-            async authorize(credentials) {
-                // Validates credentials, returning user object if valid and null otherwise
-                let loginResponse = null;
-
-                // Type safety guard
-                if((credentials?.username !== undefined) && (credentials?.password !== undefined)) {
-                    loginResponse = await validate_credentials(credentials.username, credentials.password);
-                }
-                
-                if (loginResponse?.success) {
-                    const user = {
-                        username: loginResponse.username,
-                        account_type: loginResponse.account_type,
-                        id: loginResponse.id
-                    }
-                    return user;
-                } 
-                
-                console.log(`Login error: ${loginResponse?.error}`);
-                return null;
-            }
-        }),
-    ],
-    session: {
-        maxAge: 1 * 60 * 60, // 1 hour
-    },
-    callbacks: {
-        async jwt({token, user}) {
-            if(user) {
-                token.username = user.username;
-                token.account_type = user.account_type;
-                token.id = user.id;
-            }
-
-            return token;
-        },
-        async session({session, token}) {
-            if(token) {
-                session.user.username = token.username;
-                session.user.account_type = token.account_type;
-                session.user.id = token.id;
-            }
-            return session;
-        }
-    },
-}
+import { authOptions } from "@/app/lib/auth/authOptions";
 
 const handler = NextAuth(authOptions)
 
--- frontend/app/lib/account/authSession.tsx
+++ frontend/app/lib/account/authSession.tsx
@@ -10,7 +10,7 @@ Description:
 "use server"
 
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/lib/auth/authOptions";
 
 /**
  * Validates the active session by passing {@link authOptions} to {@link getServerSession},
